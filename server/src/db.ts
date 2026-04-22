a7ccf892cce9f275f84e2b8686a9a7952f41b698
Giancarlo Bajit <gbajit514@gmail.com>
2026-04-22 14:12:51 +0800

    chore(server): add connection guard for Mongoose readyState

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,6 +1,7 @@
 import mongoose from 'mongoose';
 
 export async function connectDB(uri: string): Promise<void> {
+  if (mongoose.connection.readyState !== 0) return;
   await mongoose.connect(uri);
   console.log('MongoDB connected');
 }
